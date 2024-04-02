d757967990919f9176523ae70451e73b7378f792
Emil Sukiennik <ewilemil@gmail.com>
2024-04-02 23:14:07 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const list = document.querySelector("ul");
+const listItems = document.querySelectorAll("li");
+const sortList = ()=>{
+    [
+        ...listItems
+    ].sort((a, b)=>toNumber(b.dataset.salary) - toNumber(a.dataset.salary)).map((employee)=>list.append(employee));
+};
+const toNumber = (salary)=>+salary.replace(/[$,]/g, "");
+const getEmployees = ()=>{
+    [
+        ...listItems
+    ].map((employee)=>({
+            name: employee.textContent,
+            position: employee.dataset.position,
+            salary: employee.dataset.salary,
+            age: employee.dataset.age
+        }));
+};
+sortList();
+getEmployees();
+
+//# sourceMappingURL=index.f75de5e1.js.map
